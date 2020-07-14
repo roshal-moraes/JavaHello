303db87f9d2a52d1db34c35f5c25ffe0737cbb56
= <=>
2020-07-14 12:25:54 +0530

    Hello World in Java

---
+++ App.java
@@ -0,0 +1,14 @@
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+	//Logger logger = LoggerFactory.getLogger(App.class);
+	//logger.info("Hello World!");
+    }
+}
